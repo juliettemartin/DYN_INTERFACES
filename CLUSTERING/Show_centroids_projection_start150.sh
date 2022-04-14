de87964ac64744f417ec495666bb5a3ee627d2a3
juliettemartin <juliettemart1@gmail.com>
2022-04-14 13:16:08 +0200

    Add files via upload

--- CLUSTERING/Show_centroids_projection_start150.sh
+++ CLUSTERING/Show_centroids_projection_start150.sh
@@ -6,18 +6,15 @@ nb=`echo $line| awk '{print $2}' `
 
 awk 'BEGIN{FS=","}{printf $3" "}'  ../CONTACT_ANALYSIS/CENTROID_V1/Cluster_centroid_$complex\_$nb.txt  > centroids-v1.temp 
 
-grep ^$nb centroids_$complex.txt | cut -d" " -f2- > centroids-v2.temp
+grep ^$nb centroids_start_150_$complex.txt | cut -d" " -f2- > centroids-v2.temp
 
 echo Jacquard_index_$complex.txt > input.txt
 echo $complex >> input.txt
 echo centroids-v1.temp >> input.txt 
 echo centroids-v2.temp >> input.txt 
 
-
-
 R CMD BATCH show_centroids_projection.R
-mv Rplots.pdf Projection_$complex\_$nb.pdf
-mv ProjectionV2.pdf ProjectionV2_$complex\_$nb.pdf
+mv ProjectionV2.pdf ProjectionV2_$complex\_$nb\_start150.pdf
 
-done < Nb_clusters_1PVH.txt
+done < Nb_clusters.txt
 
