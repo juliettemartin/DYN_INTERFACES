b2d4f81e01156ba25c8b43feab621bf8799a3a85
juliettemartin <juliettemart1@gmail.com>
2022-04-28 14:44:08 +0200

    call shorter version of R script

--- CLUSTERING/plot_dist_matrix.sh
+++ CLUSTERING/plot_dist_matrix.sh
@@ -2,7 +2,7 @@ for complex in 1PVH #3SGB 1FFW 1BRS 1EMV  1GCQ  2J0T 2OOB  1AK4 1AY7
 do
 echo Jacquard_index_$complex.txt > input.txt
 echo $complex >> input.txt
-R CMD BATCH plot_dist_matrix.R
+R CMD BATCH plot_dist_matrix_short.R
 mv Rplots.pdf Sim_matrix_$complex.pdf
 mv hclust.pdf hclust_$complex.pdf
 mv Membership.pdf Membership_$complex.pdf
