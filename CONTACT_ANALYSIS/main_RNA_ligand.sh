e2ab217fa3f108f96e7c79f0b50968404a02c748
juliette martin <juliette.martin@ibcp.fr>
2022-06-21 12:24:16 +0200

    RNA/ligand case

---
+++ CONTACT_ANALYSIS/main_RNA_ligand.sh
@@ -0,0 +1,17 @@
+# activate envt
+source activate dyn_interfaces 
+
+# Store contacts in pkl objects
+echo "######"
+echo "running compute_contacts.sh"
+source compute_contacts_RNA_lig.sh 
+
+# Compute contact frequency
+echo "######"
+echo "running contact_frequency.sh"
+source contact_frequency.sh
+
+# Plot contact frequency
+echo "######"
+echo "running plot_frequency.sh"
+source plot_frequency.sh
